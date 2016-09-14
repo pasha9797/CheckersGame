9c3c4fdfd4bcc438c1d435d58745f943eed02c1e
pasha9797 <pasha.97.97@mail.ru>
2016-09-14 08:09:40 +0300

    jhu

--- CheckersGame/CheckersGame/Form1.cs
+++ CheckersGame/CheckersGame/Form1.cs
@@ -29,7 +29,7 @@ namespace CheckersGame
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             myBoard.myDraw(g);
-
+            hhh
         }
 
         private void Form1_MouseDown(object sender, MouseEventArgs e)
